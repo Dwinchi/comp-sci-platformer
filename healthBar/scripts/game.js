09fa0f41eb4b0e2fb522f99d51c94130b5325c3b
angel-gamis <113718007+angel-gamis@users.noreply.github.com>
2022-12-16 13:14:20 -0600

    Updated

--- healthBar/scripts/game.js
+++ healthBar/scripts/game.js
@@ -1,16 +1,16 @@
 // Canvas Variables
 const cUI = document.getElementById("ui-layer");
 const ctxUI = cUI.getContext("2d");
-const width = cUI.width = 980;
-const height = cUI.height = 540;
+const width = cUI.width = 1280;
+const height = cUI.height = 720;
 
 // Health Bar Variables
 let maxHealth = 100;
 let health = 100;
-const healthBarWidth = 65;
-const healthBarHeight = 7;
-const x = 50;
-const y = 8;
+const healthBarWidth = 192;
+const healthBarHeight = 19;
+const x = 199;
+const y = 26;
 
 const healthBar = new HealthBar(x, y, healthBarWidth, healthBarHeight, maxHealth, health,  "green");
 
@@ -19,16 +19,18 @@ const frame = function() {
   ctxUI.clearRect(0, 0, width, height);
   let baseLayer = new Image();
   baseLayer.src = 'images/baseLayer.png';
-  ctxUI.drawImage(baseLayer, 45, 4, 75, 15);
+  ctxUI.drawImage(baseLayer, 180, 16, 225, 45);
   healthBar.show(ctxUI);
   let border = new Image();
   border.src = 'images/border.png';
-  ctxUI.drawImage(border, 45, 4, 75, 15);
+  ctxUI.drawImage(border, 180, 16, 225, 45);
   let heart = new Image();
   heart.src = 'images/heart.png';
-  ctxUI.drawImage(heart, 45, 4, 75, 15);
+  ctxUI.drawImage(heart, 180, 16, 225, 45);
   requestAnimationFrame(frame);
 }
+
+frame();
 
 //  ** Change to a on damage function **
 cUI.onclick = function() {
@@ -38,5 +40,3 @@ cUI.onclick = function() {
     healthBar.updateColor(health);
   }
 };
-
-frame();
